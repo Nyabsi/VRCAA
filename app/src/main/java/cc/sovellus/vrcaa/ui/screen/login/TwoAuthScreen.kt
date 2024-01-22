e21ec726071560cfd151570defdc4c0e45f04afc
nyabsi <nyabsi@sovellus.cc>
2024-01-22 19:34:27 +0200

    [CI] redesign TwoAuthScreen and workaround nasty Android bug til it's fixed.

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/login/TwoAuthScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/login/TwoAuthScreen.kt
@@ -1,26 +1,47 @@
 package cc.sovellus.vrcaa.ui.screen.login
 
+import android.media.effect.Effect
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.lifecycle.LifecycleEffect
 import cafe.adriel.voyager.core.model.rememberNavigatorScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import cc.sovellus.vrcaa.api.ApiContext
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TwoAuthScreen(
    private val token: String
@@ -64,7 +85,9 @@ class TwoAuthScreen(
         }
 
         Column (
-            modifier = Modifier.fillMaxSize().padding(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -74,19 +97,63 @@ class TwoAuthScreen(
         }
     }
 
+    @OptIn(ExperimentalLayoutApi::class)
     @Composable
     fun TextInput(
         title: String,
         input: MutableState<String>
     ) {
-        OutlinedTextField(
+        val focus = LocalFocusManager.current
+        val coroutineScope = rememberCoroutineScope()
+
+        BasicTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(16.dp)
+                .onFocusChanged {
+                    coroutineScope.launch {
+                        delay(5000)
+                        focus.clearFocus()
+                    }
+                },
             value = input.value,
-            onValueChange = { input.value = it },
-            label = { Text(text = title) },
+            onValueChange = {
+                if (it.length <= 6)
+                    input.value = it
+            },
             singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.NumberPassword
+            ),
+            decorationBox = {
+                Row(
+                    horizontalArrangement = Arrangement
+                        .spacedBy(
+                            space = 16.dp,
+                            alignment = Alignment.CenterHorizontally
+                        )
+                ) {
+                    repeat(6) {
+                        val char = when {
+                            it >= input.value.length -> ""
+                            else -> input.value[it].toString()
+                        }
+                        
+                        Text(
+                            modifier = Modifier
+                                .width(50.dp)
+                                .height(60.dp)
+                                .border(1.dp, Color.White, RoundedCornerShape(8.dp))
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .padding(top = 10.dp),
+                            text = char,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.h4
+                        )
+                    }
+                }
+            }
         )
     }
 }
