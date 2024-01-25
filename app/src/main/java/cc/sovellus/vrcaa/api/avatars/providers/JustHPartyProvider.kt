3eedb6becc872c8e23321ab97d07e65507371c88
nyabsi <nyabsi@sovellus.cc>
2024-01-25 23:47:06 +0200

    Fix referer

--- app/src/main/java/cc/sovellus/vrcaa/api/avatars/providers/JustHPartyProvider.kt
+++ app/src/main/java/cc/sovellus/vrcaa/api/avatars/providers/JustHPartyProvider.kt
@@ -7,10 +7,9 @@ import java.net.URLEncoder
 class JustHPartyProvider : AvatarProviderContext() {
     override val apiUrl = "https://avtr.just-h.party/vrcx_search.php"
     override val userAgent = "VRCAA/0.1"
-    override val referer = "https://vrcaa.sovellus.cc"
+    override val referer = "vrcaa.sovellus.cc"
 
     suspend fun search(type: String, query: String, n: Int) {
         val result = sendRequest("?${type}=${URLEncoder.encode(query)}&n=5000")
-        Log.d("VRCAA", result.toString())
     }
 }
