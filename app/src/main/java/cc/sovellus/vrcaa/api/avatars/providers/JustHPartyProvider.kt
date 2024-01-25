54d375d81ba6d61e29798378acbd103fac1d41aa
nyabsi <nyabsi@sovellus.cc>
2024-01-25 20:28:27 +0200

    Implement Initial AvatarProviderContext (unused, for now)

---
+++ app/src/main/java/cc/sovellus/vrcaa/api/avatars/providers/JustHPartyProvider.kt
@@ -0,0 +1,16 @@
+package cc.sovellus.vrcaa.api.avatars.providers
+
+import android.util.Log
+import cc.sovellus.vrcaa.api.avatars.AvatarProviderContext
+import java.net.URLEncoder
+
+class JustHPartyProvider : AvatarProviderContext() {
+    override val apiUrl = "https://avtr.just-h.party/vrcx_search.php"
+    override val userAgent = "VRCAA/0.1"
+    override val referer = "https://vrcaa.sovellus.cc"
+
+    suspend fun search(type: String, query: String, n: Int) {
+        val result = sendRequest("?${type}=${URLEncoder.encode(query)}&n=5000")
+        Log.d("VRCAA", result.toString())
+    }
+}
