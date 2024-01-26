660fc0f32d4f590b8f37ce0e7224474504ed6e36
nyabsi <nyabsi@sovellus.cc>
2024-01-26 09:48:56 +0200

    Finalize JustHPartyProvider for Avatars

--- app/src/main/java/cc/sovellus/vrcaa/api/avatars/providers/JustHPartyProvider.kt
+++ app/src/main/java/cc/sovellus/vrcaa/api/avatars/providers/JustHPartyProvider.kt
@@ -1,7 +1,8 @@
 package cc.sovellus.vrcaa.api.avatars.providers
 
-import android.util.Log
 import cc.sovellus.vrcaa.api.avatars.AvatarProviderContext
+import cc.sovellus.vrcaa.api.avatars.models.JustHPartyAvatars
+import com.google.gson.Gson
 import java.net.URLEncoder
 
 class JustHPartyProvider : AvatarProviderContext() {
@@ -9,7 +10,12 @@ class JustHPartyProvider : AvatarProviderContext() {
     override val userAgent = "VRCAA/0.1"
     override val referer = "vrcaa.sovellus.cc"
 
-    suspend fun search(type: String, query: String, n: Int) {
-        val result = sendRequest("?${type}=${URLEncoder.encode(query)}&n=5000")
+    suspend fun search(type: String, query: String, n: Int): JustHPartyAvatars? {
+        return when (val result = sendRequest("?${type}=${URLEncoder.encode(query)}&n=5000")) {
+            is String -> {
+                Gson().fromJson(result, JustHPartyAvatars::class.java)
+            }
+            else -> null
+        }
     }
 }
