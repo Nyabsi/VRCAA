6b6856977a026c8639a9570fbd781f234c48bdc8
nyabsi <nyabsi@sovellus.cc>
2024-02-06 16:47:41 +0200

    Get rid off compiler warnings

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/misc/NestedPlaceholderScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/misc/NestedPlaceholderScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -39,7 +39,7 @@ class NestedPlaceholderScreen : Screen {
                     navigationIcon = {
                         IconButton(onClick = { navigator.pop() }) {
                             Icon(
-                                imageVector = Icons.Filled.ArrowBack,
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                                 contentDescription =stringResource(R.string.preview_image_description)
                             )
                         }
