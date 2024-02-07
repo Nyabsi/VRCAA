f3fa7352b2a229bdf757c30d845c65cf5e630aff
nyabsi <nyabsi@sovellus.cc>
2024-02-07 18:32:39 +0200

    Cleanup warnings from last update pt3

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/main/MainScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/main/MainScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
