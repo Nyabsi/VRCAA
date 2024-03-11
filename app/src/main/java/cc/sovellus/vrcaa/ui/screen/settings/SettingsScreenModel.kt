90b107be26a3ea0806fce58296e03dddeaa5ff8e
nyabsi <nyabsi@sovellus.cc>
2024-03-11 12:48:19 +0200

    Minor adjustments to logout

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/settings/SettingsScreenModel.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/settings/SettingsScreenModel.kt
@@ -1,12 +1,11 @@
 package cc.sovellus.vrcaa.ui.screen.settings
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
-import cc.sovellus.vrcaa.api.ApiContext
-import cc.sovellus.vrcaa.helper.isMyServiceRunning
+import cc.sovellus.vrcaa.api.http.ApiContext
+import cc.sovellus.vrcaa.helper.cookies
 import cc.sovellus.vrcaa.service.PipelineService
 import kotlinx.coroutines.launch
 
@@ -14,20 +13,14 @@ class SettingsScreenModel(
     private val context: Context
 ) : ScreenModel {
     private val api = ApiContext(context)
+    private val preferences = context.getSharedPreferences("vrcaa_prefs", 0)
 
-    @SuppressLint("ApplySharedPref")
     fun doLogout() {
         screenModelScope.launch {
             api.logout()
-
-            if (context.isMyServiceRunning(PipelineService::class.java)) {
-                val intent = Intent(context, PipelineService::class.java)
-                context.stopService(intent)
-            }
-
-            val editor = context.getSharedPreferences("vrcaa_prefs", 0).edit()
-            editor.clear()
-            editor.commit()
+            val intent = Intent(context, PipelineService::class.java)
+            context.stopService(intent)
+            preferences.cookies = ""
         }
     }
 }
