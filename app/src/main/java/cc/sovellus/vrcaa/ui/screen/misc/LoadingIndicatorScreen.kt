c2d3fd3dad8ae96c46657022e9269da456461506
nyabsi <nyabsi@sovellus.cc>
2024-04-12 10:50:51 +0300

    LoadingIndicatorScreen: change indicator to linear and add ih8sn label

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/misc/LoadingIndicatorScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/misc/LoadingIndicatorScreen.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cc.sovellus.vrcaa.R
 
 class LoadingIndicatorScreen : Screen {
 
@@ -21,12 +25,18 @@ class LoadingIndicatorScreen : Screen {
     @Composable
     override fun Content() {
         Column(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            CircularProgressIndicator(
-                modifier = Modifier.width(64.dp),
+            Text(
+                text = stringResource(R.string.misc_loading_label_text),
+                fontWeight = FontWeight.Normal
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.width(128.dp),
                 color = MaterialTheme.colorScheme.secondary,
                 trackColor = MaterialTheme.colorScheme.surfaceVariant,
             )
