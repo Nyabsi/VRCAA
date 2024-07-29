c114248069f30c32f4df1dfb942c314ff02f717b
Nyabsi <nyabsi@sovellus.cc>
2024-07-29 22:02:34 +0300

    Separate friends on instances from ones in private

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/friends/FriendsScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/friends/FriendsScreen.kt
@@ -1,6 +1,5 @@
 package cc.sovellus.vrcaa.ui.screen.friends
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,6 +17,7 @@ import androidx.compose.material.icons.filled.PersonOff
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.Web
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MultiChoiceSegmentedButtonRow
 import androidx.compose.material3.SegmentedButton
@@ -26,12 +26,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -121,8 +118,11 @@ class FriendsScreen : Screen {
     fun ShowFriendsFavorite(
         friends: State<List<Friend>>
     ) {
-        val filteredFriends = friends.value.filter { it.isFavorite }
-        if (filteredFriends.isEmpty()) {
+        val favoriteFriends = friends.value.filter { it.isFavorite && it.location.contains("wrld_").not() && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val favoriteFriendsInInstances = friends.value.filter { it.isFavorite && it.location.contains("wrld_") && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val favoriteFriendsOffline = friends.value.filter { it.isFavorite && StatusHelper.getStatusFromString(it.status) == StatusHelper.Status.Offline }
+
+        if (favoriteFriends.isEmpty() && favoriteFriendsInInstances.isEmpty() && favoriteFriendsOffline.isEmpty()) {
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
@@ -139,7 +139,56 @@ class FriendsScreen : Screen {
                     .padding(1.dp),
                 state = rememberLazyListState()
             ) {
-                items(filteredFriends.sortedBy { StatusHelper.getStatusFromString(it.status) }, key = { UUID.randomUUID() }) { friend ->
+                if (favoriteFriendsInInstances.isNotEmpty()) {
+                    items(
+                        favoriteFriendsInInstances.sortedBy { StatusHelper.getStatusFromString(it.status) },
+                        key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+
+                    item {
+                        HorizontalDivider(
+                            color = Color.Gray,
+                            thickness = 0.5.dp
+                        )
+                    }
+
+                    items(
+                        favoriteFriends.sortedBy { StatusHelper.getStatusFromString(it.status) },
+                        key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+
+                } else {
+                    items(
+                        favoriteFriends.sortedBy { StatusHelper.getStatusFromString(it.status) },
+                        key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+                }
+
+                if (favoriteFriendsOffline.isNotEmpty()) {
+                    if (favoriteFriends.isNotEmpty())
+                        item {
+                            HorizontalDivider(
+                                color = Color.Gray,
+                                thickness = 0.5.dp
+                            )
+                        }
+                }
+
+                items(
+                    favoriteFriendsOffline.sortedBy { StatusHelper.getStatusFromString(it.status) },
+                    key = { UUID.randomUUID() }) { friend ->
                     FriendItem(
                         friend = friend,
                         callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
@@ -153,41 +202,7 @@ class FriendsScreen : Screen {
     fun ShowFriendsOnWebsite(
         friends: State<List<Friend>>
     ) {
-        val filteredFriends = friends.value.sortedBy { StatusHelper.getStatusFromString(it.status) }
-        if (filteredFriends.isEmpty()) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = stringResource(R.string.result_not_found))
-            }
-        } else {
-            val navigator = LocalNavigator.currentOrThrow
-            LazyColumn(
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(1.dp),
-                state = rememberLazyListState()
-            ) {
-                items(filteredFriends, key = { UUID.randomUUID() }) { friend ->
-                    if (friend.location == "offline" && StatusHelper.getStatusFromString(friend.status) != StatusHelper.Status.Offline) {
-                        FriendItem(
-                            friend = friend,
-                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun ShowFriends(
-        friends: State<List<Friend>>
-    ) {
-        val filteredFriends = friends.value.filter { !it.isFavorite && it.location != "offline" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val filteredFriends = friends.value.filter { it.location == "offline" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
         if (filteredFriends.isEmpty()) {
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -211,6 +226,63 @@ class FriendsScreen : Screen {
                         callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
                     )
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun ShowFriends(
+        friends: State<List<Friend>>
+    ) {
+        val filteredFriends = friends.value.filter { !it.isFavorite && it.location != "offline" && it.location.contains("wrld_").not() && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val filteredFriendsInInstances = friends.value.filter { !it.isFavorite && it.location.contains("wrld_") && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+
+        if (filteredFriends.isEmpty() && filteredFriendsInInstances.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(R.string.result_not_found))
+            }
+        } else {
+            val navigator = LocalNavigator.currentOrThrow
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(1.dp),
+                state = rememberLazyListState()
+            ) {
+                if (filteredFriendsInInstances.isNotEmpty())
+                {
+                    items(filteredFriendsInInstances.sortedBy { StatusHelper.getStatusFromString(it.status)  }, key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+                    item {
+                        HorizontalDivider(
+                            color = Color.Gray,
+                            thickness = 0.5.dp
+                        )
+                    }
+                    items(filteredFriends.sortedBy { StatusHelper.getStatusFromString(it.status)  }, key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+                } else {
+                    items(filteredFriends.sortedBy { StatusHelper.getStatusFromString(it.status)  }, key = { UUID.randomUUID() }) { friend ->
+                        FriendItem(
+                            friend = friend,
+                            callback = { navigator.parent?.parent?.push(UserProfileScreen(friend.id)) }
+                        )
+                    }
+                }
+
             }
         }
     }
