7c60c26a5289d15f2fbaf03941cbb0a039baa700
Nyabsi <nyabsi@sovellus.cc>
2024-07-29 19:03:55 +0300

    Fix login screen not loading with invalid token

--- app/src/main/java/cc/sovellus/vrcaa/activity/MainActivity.kt
+++ app/src/main/java/cc/sovellus/vrcaa/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -72,7 +73,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Content(invalidSession == null || terminateSession == null)
+                    Content(invalidSession == null && terminateSession == null && token.isNotBlank())
                 }
             }
         }
