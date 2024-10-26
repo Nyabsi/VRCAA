a59862d29bfa88846bcc560a5fa37fbc4e573d00
Nyabsi <nyabsi@sovellus.cc>
2024-10-26 13:11:39 +0300

    PipelineService: set scheduler "INITIAL_INTERVAL" to 0

--- app/src/main/java/cc/sovellus/vrcaa/service/PipelineService.kt
+++ app/src/main/java/cc/sovellus/vrcaa/service/PipelineService.kt
@@ -397,7 +397,7 @@ class PipelineService : Service(), CoroutineScope {
 
     companion object {
         private const val NOTIFICATION_ID: Int = 42069
-        private const val INITIAL_INTERVAL: Long = 1000
+        private const val INITIAL_INTERVAL: Long = 0
         private const val RESTART_INTERVAL: Long = 1800000
     }
 }
