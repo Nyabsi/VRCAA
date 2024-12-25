66ecf33f5fd5346897f76bdb7ac78137a044e565
Nyabsi <nyabsi@sovellus.cc>
2024-12-25 17:48:14 +0200

    do full friend state copy, field for field
    (cherry picked from commit 3694d2dfc0efa72f6ce1ee24cfdae8161ca157a1)

--- app/src/main/java/cc/sovellus/vrcaa/manager/FriendManager.kt
+++ app/src/main/java/cc/sovellus/vrcaa/manager/FriendManager.kt
@@ -50,7 +50,29 @@ object FriendManager {
     fun updateFriend(friend: Friend) {
         val it = friends.find { it.id == friend.id }
         it?.let {
-            friends.set(friends.indexOf(it), friend)
+            it.bio = friend.bio
+            it.bioLinks = friend.bioLinks
+            it.currentAvatarImageUrl = friend.currentAvatarImageUrl
+            it.currentAvatarTags = friend.currentAvatarTags
+            it.currentAvatarThumbnailImageUrl = friend.currentAvatarThumbnailImageUrl
+            it.developerType = friend.developerType
+            it.displayName = friend.displayName
+            it.friendKey = friend.friendKey
+            it.id = friend.id
+            it.imageUrl = friend.imageUrl
+            it.isFriend = friend.isFriend
+            it.lastLogin = it.lastLogin
+            it.lastMobile = it.lastMobile
+            it.lastPlatform = friend.lastPlatform
+            it.location = friend.location
+            it.profilePicOverride = friend.profilePicOverride
+            it.profilePicOverrideThumbnail = friend.profilePicOverrideThumbnail
+            it.status = friend.status
+            it.statusDescription = friend.statusDescription
+            it.tags = friend.tags
+            it.userIcon = friend.userIcon
+            it.platform = friend.platform
+            friends.set(friends.indexOf(it), it)
         }
 
         friendListeners.map {
