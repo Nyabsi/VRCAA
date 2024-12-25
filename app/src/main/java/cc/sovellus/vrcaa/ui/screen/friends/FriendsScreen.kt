2e4fd37ffe4002a5978cb77ca196f9b0501c24d5
Nyabsi <nyabsi@sovellus.cc>
2024-12-25 18:10:23 +0200

    use status to determine friend status and just update the entire friend model in same go

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/friends/FriendsScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/friends/FriendsScreen.kt
@@ -127,9 +127,9 @@ class FriendsScreen : Screen {
     fun ShowFriendsFavorite(
         friends: State<List<Friend>>
     ) {
-        val favoriteFriends = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_").not() && it.platform != "web" && it.platform != "" }
-        val favoriteFriendsInInstances = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_") && it.platform != "web" && it.platform != "" }
-        val favoriteFriendsOffline = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && it.platform == "" }
+        val favoriteFriends = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_").not() && it.platform != "web" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val favoriteFriendsInInstances = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_") && it.platform != "web" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val favoriteFriendsOffline = friends.value.filter { FavoriteManager.isFavorite("friend", it.id) && StatusHelper.getStatusFromString(it.status) == StatusHelper.Status.Offline }
 
         if (favoriteFriends.isEmpty() && favoriteFriendsInInstances.isEmpty() && favoriteFriendsOffline.isEmpty()) {
             Column(
@@ -207,7 +207,7 @@ class FriendsScreen : Screen {
     fun ShowFriendsOnWebsite(
         friends: State<List<Friend>>
     ) {
-        val filteredFriends = friends.value.filter { it.platform == "web" }
+        val filteredFriends = friends.value.filter { it.platform == "web" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
         if (filteredFriends.isEmpty()) {
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -239,8 +239,8 @@ class FriendsScreen : Screen {
     fun ShowFriends(
         friends: State<List<Friend>>
     ) {
-        val filteredFriends = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_").not() && it.platform != "" && it.platform != "web" }
-        val filteredFriendsInInstances = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_") && it.platform != "" && it.platform != "web" }
+        val filteredFriends = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_").not() && it.platform != "web" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
+        val filteredFriendsInInstances = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && it.location.contains("wrld_") && it.platform != "web" && StatusHelper.getStatusFromString(it.status) != StatusHelper.Status.Offline }
 
         if (filteredFriends.isEmpty() && filteredFriendsInInstances.isEmpty()) {
             Column(
@@ -296,7 +296,7 @@ class FriendsScreen : Screen {
     fun ShowFriendsOffline(
         friends: State<List<Friend>>
     ) {
-        val filteredFriends = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && it.platform == "" }
+        val filteredFriends = friends.value.filter { !FavoriteManager.isFavorite("friend", it.id) && StatusHelper.getStatusFromString(it.status) == StatusHelper.Status.Offline }
         if (filteredFriends.isEmpty()) {
             Column(
                 modifier = Modifier.fillMaxSize(),
