fd8564c09dc7d9e710e8c909d07711e6ed5f422d
Nyabsi <nyabsi@sovellus.cc>
2024-12-28 17:23:43 +0200

    add missing imports to AvatarScreen

--- app/src/main/java/cc/sovellus/vrcaa/ui/screen/avatar/AvatarScreen.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/avatar/AvatarScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
