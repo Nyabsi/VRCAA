c9a200a7e4c8f32a13ac4f1145c3023fd0cea276
Nyabsi <nyabsi@sovellus.cc>
2025-01-03 12:59:12 +0200

    move platform badges above image instead of after title

--- app/src/main/java/cc/sovellus/vrcaa/ui/components/card/WorldCard.kt
+++ app/src/main/java/cc/sovellus/vrcaa/ui/components/card/WorldCard.kt
@@ -1,6 +1,7 @@
 package cc.sovellus.vrcaa.ui.components.card
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -28,6 +30,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import cc.sovellus.vrcaa.R
 import cc.sovellus.vrcaa.api.vrchat.http.models.World
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -75,16 +78,73 @@ fun WorldCard(
                 .fillMaxSize()
         ) {
             item {
-                GlideImage(
-                    model = world.thumbnailImageUrl,
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier
+                Box(
+                    Modifier
                         .fillMaxWidth()
                         .height(160.dp),
-                    contentScale = ContentScale.Crop,
-                    loading = placeholder(R.drawable.image_placeholder),
-                    failure = placeholder(R.drawable.image_placeholder)
-                )
+                    contentAlignment = Alignment.TopStart
+                ) {
+                    GlideImage(
+                        model = world.thumbnailImageUrl,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(160.dp)
+                            .zIndex(0f),
+                        contentScale = ContentScale.Crop,
+                        loading = placeholder(R.drawable.image_placeholder),
+                        failure = placeholder(R.drawable.image_placeholder)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 8.dp)
+                            .zIndex(1f),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        if (foundWindows) {
+                            Badge(
+                                containerColor = Color(0, 168, 252, 191),
+                                modifier = Modifier
+                                    .height(height = 26.dp)
+                                    .padding(start = 2.dp, top = 8.dp),
+                                content = {
+                                    Text(
+                                        text = "Windows"
+                                    )
+                                }
+                            )
+                        }
+
+                        if (foundAndroid) {
+                            Badge(
+                                containerColor = Color(103, 215, 129, 191),
+                                modifier = Modifier
+                                    .height(height = 26.dp)
+                                    .padding(start = 2.dp, top = 8.dp),
+                                content = {
+                                    Text(
+                                        text = "Android"
+                                    )
+                                }
+                            )
+                        }
+
+                        if (foundDarwin) {
+                            Badge(
+                                containerColor = Color(121, 136, 151, 191),
+                                modifier = Modifier
+                                    .height(height = 26.dp)
+                                    .padding(start = 2.dp, top = 8.dp),
+                                content = {
+                                    Text(
+                                        text = "iOS"
+                                    )
+                                }
+                            )
+                        }
+                    }
+                }
             }
 
             item {
@@ -99,53 +159,6 @@ fun WorldCard(
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(end = 4.dp), horizontalArrangement = Arrangement.End
-                    ) {
-                        if (foundWindows) {
-                            Badge(
-                                containerColor = Color(0, 168, 252),
-                                modifier = Modifier
-                                    .height(height = 26.dp)
-                                    .padding(start = 2.dp, top = 8.dp),
-                                content = {
-                                    Text(
-                                        text = "Windows"
-                                    )
-                                }
-                            )
-                        }
-
-                        if (foundAndroid) {
-                            Badge(
-                                containerColor = Color(103, 215, 129),
-                                modifier = Modifier
-                                    .height(height = 26.dp)
-                                    .padding(start = 2.dp, top = 8.dp),
-                                content = {
-                                    Text(
-                                        text = "Android"
-                                    )
-                                }
-                            )
-                        }
-
-                        if (foundDarwin) {
-                            Badge(
-                                containerColor = Color(121, 136, 151),
-                                modifier = Modifier
-                                    .height(height = 26.dp)
-                                    .padding(start = 2.dp, top = 8.dp),
-                                content = {
-                                    Text(
-                                        text = "iOS"
-                                    )
-                                }
-                            )
-                        }
-                    }
                 }
             }
 
