43a55827aa8242075bc9a7354def3d77c3d12730
Nyabsi <nyabsi@sovellus.cc>
2025-06-02 22:49:06 +0300

    [Skip-CI] version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.sovellus.vrcaa"
         minSdk = 27
         targetSdk = 35
-        versionCode = 200500
-        versionName = "2.5.0"
+        versionCode = 200501
+        versionName = "2.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
