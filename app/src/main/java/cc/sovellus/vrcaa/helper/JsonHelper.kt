fa8d5ebd10eb9d1c2936709fc48e67d7fd466eed
Nyabsi <nyabsi@sovellus.cc>
2025-07-04 14:27:59 +0300

    api: implement json merge for data consistency
    (cherry picked from commit 9acce2068e6578d3510aa23901c4bdb5eeda9f7f)

---
+++ app/src/main/java/cc/sovellus/vrcaa/helper/JsonHelper.kt
@@ -0,0 +1,37 @@
+package cc.sovellus.vrcaa.helper
+
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+
+object JsonHelper {
+    fun <T> mergeJson(old: T, new: T, type: Class<T>): T {
+        val merged = mergeObjects(JsonParser.parseString(Gson().toJson(old)).asJsonObject, JsonParser.parseString(Gson().toJson(new)).asJsonObject)
+        return Gson().fromJson(merged, type)
+    }
+
+    private fun mergeObjects(old: JsonObject, new: JsonObject): JsonObject {
+        val temp = old.deepCopy()
+        for (field in old.entrySet()) {
+            if (new.has(field.key)) {
+                val value = new.get(field.key)
+                if (isFieldEmptyOrNull(value))
+                    continue
+                temp.remove(field.key)
+                temp.add(field.key, value)
+            }
+        }
+        return temp
+    }
+
+    private fun isFieldEmptyOrNull(obj: JsonElement): Boolean {
+        if (obj.isJsonNull)
+            return true
+        else if (obj.isJsonObject && (obj.asJsonObject.isEmpty || obj.asJsonObject.isJsonNull))
+            return true
+        else if (obj.isJsonArray && (obj.asJsonArray.isEmpty || obj.asJsonArray.isJsonNull))
+            return true
+        return false
+    }
+}
