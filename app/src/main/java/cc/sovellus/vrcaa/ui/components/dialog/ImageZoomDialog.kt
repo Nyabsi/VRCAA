18b66235432779c964cc625a2a66da169790203b
Nyabsi <nyabsi@sovellus.cc>
2025-07-06 02:38:57 +0300

    feat: implement Gallery preview + image downloading
    (cherry picked from commit fae97ce7d4e7a4f8d6d34908c4f42c03575c9203)

---
+++ app/src/main/java/cc/sovellus/vrcaa/ui/components/dialog/ImageZoomDialog.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2025. Nyabsi <nyabsi@sovellus.cc>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.sovellus.vrcaa.ui.components.dialog
+
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import cc.sovellus.vrcaa.App
+import cc.sovellus.vrcaa.ui.components.misc.ZoomableImage
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private fun saveGlideImageToPickedUri(imageUrl: String, pickedUri: Uri) {
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val file = Glide.with(App.getContext())
+                .asFile()
+                .load(imageUrl)
+                .submit()
+                .get()
+
+            App.getContext().contentResolver.openOutputStream(pickedUri)?.use { outputStream ->
+                file.inputStream().copyTo(outputStream)
+            }
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    App.getContext(),
+                    "Saved picture.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    App.getContext(),
+                    "Unable to save picture.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ImageZoomDialog(
+    name: String,
+    url: String,
+    onDismiss: () -> Unit
+) {
+    var pendingImageUrl by remember { mutableStateOf<String?>(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.CreateDocument("image/jpeg")) { uri ->
+        uri?.let {
+            pendingImageUrl?.let { imageUrl ->
+                saveGlideImageToPickedUri(imageUrl, it)
+            }
+            pendingImageUrl = null
+        }
+    }
+
+    Dialog(onDismissRequest = onDismiss) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            containerColor = Color.Transparent,
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors().copy(containerColor = Color.Transparent),
+                    navigationIcon = {
+                        IconButton(onClick = { onDismiss() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            pendingImageUrl = url
+                            launcher.launch("${name}.png")
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Download,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    title = {
+                        Text(text = "Preview")
+                    }
+                )
+            }
+        ) { padding ->
+            Box(
+                modifier = Modifier.padding(padding)
+            ) {
+                ZoomableImage(
+                    imageUrl = url,
+                    modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets(0))
+                )
+            }
+        }
+    }
+}
