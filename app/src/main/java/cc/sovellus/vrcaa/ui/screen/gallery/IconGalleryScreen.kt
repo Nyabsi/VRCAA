b2f61b6cae1f0a67f63733bd4bdfd3d2af589e03
Nyabsi <nyabsi@sovellus.cc>
2025-07-06 03:22:56 +0300

    feat: implement User Icons preview + image downloading
    (cherry picked from commit 1bcfeea9aab52498f619cd7dde818e6b06ec3bee)

---
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/gallery/IconGalleryScreen.kt
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2025. Nyabsi <nyabsi@sovellus.cc>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.sovellus.vrcaa.ui.screen.gallery
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.rememberScreenModel
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import cc.sovellus.vrcaa.R
+import cc.sovellus.vrcaa.api.vrchat.http.models.File
+import cc.sovellus.vrcaa.extension.columnCountOption
+import cc.sovellus.vrcaa.extension.fixedColumnSize
+import cc.sovellus.vrcaa.ui.components.dialog.ImageZoomDialog
+import cc.sovellus.vrcaa.ui.screen.misc.LoadingIndicatorScreen
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+
+class IconGalleryScreen : Screen {
+
+    override val key = uniqueScreenKey
+
+    @Composable
+    override fun Content() {
+        val model = rememberScreenModel { IconGalleryScreenModel() }
+        val state by model.state.collectAsState()
+
+        when (val result = state) {
+            is IconGalleryScreenModel.GalleryState.Loading -> LoadingIndicatorScreen().Content()
+            is IconGalleryScreenModel.GalleryState.Empty -> HandleEmpty()
+            is IconGalleryScreenModel.GalleryState.Result -> DisplayResult(result.files, model)
+            else -> {}
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun HandleEmpty() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = { navigator.pop() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    title = {
+                        Text(text = "Gallery")
+                    }
+                )
+            },
+            content = { padding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            top = padding.calculateTopPadding(),
+                            bottom = padding.calculateBottomPadding()
+                        ),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = stringResource(R.string.result_not_found))
+                }
+            }
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
+    @Composable
+    private fun DisplayResult(files: ArrayList<File>, model: IconGalleryScreenModel) {
+        val navigator = LocalNavigator.currentOrThrow
+
+        var previewFile by remember { mutableStateOf<File.Version.File?>(null) }
+
+        Scaffold(
+            modifier = Modifier.blur(if (previewFile != null) { 100.dp } else { 0.dp }),
+            topBar = {
+                TopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = { navigator.pop() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    title = {
+                        Text(text = "Gallery") // stringResource(R.string.avatars_page_title)
+                    }
+                )
+            },
+            content = { padding ->
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            top = padding.calculateTopPadding(),
+                            bottom = padding.calculateBottomPadding()
+                        ),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LazyVerticalGrid(
+                        columns = when (model.preferences.columnCountOption) {
+                            0 -> GridCells.Adaptive(192.dp)
+                            else -> GridCells.Fixed(model.preferences.fixedColumnSize)
+                        },
+                        contentPadding = PaddingValues(
+                            start = 12.dp,
+                            top = 16.dp,
+                            end = 16.dp,
+                            bottom = 16.dp
+                        ),
+                        content = {
+                            items(files.size) {
+                                val file = files[it]
+                                GlideImage(
+                                    model = file.versions.last().file.url,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(4.dp)
+                                        .height(192.dp)
+                                        .width(192.dp)
+                                        .clip(RoundedCornerShape(20))
+                                        .clickable(onClick = {
+                                            previewFile = file.versions.last().file
+                                        }),
+                                    contentScale = ContentScale.Crop,
+                                    loading = placeholder(R.drawable.image_placeholder),
+                                    failure = placeholder(R.drawable.image_placeholder)
+                                )
+                            }
+                        }
+                    )
+                }
+
+                previewFile?.let {
+                    ImageZoomDialog(
+                        url = it.url,
+                        name = it.fileName,
+                        onDismiss = { previewFile = null }
+                    )
+                }
+            }
+        )
+    }
+}
---
+++ app/src/main/java/cc/sovellus/vrcaa/ui/screen/gallery/IconGalleryScreenModel.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2025. Nyabsi <nyabsi@sovellus.cc>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.sovellus.vrcaa.ui.screen.gallery
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import cafe.adriel.voyager.core.model.StateScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import cc.sovellus.vrcaa.App
+import cc.sovellus.vrcaa.api.vrchat.http.models.File
+import cc.sovellus.vrcaa.manager.ApiManager.api
+import kotlinx.coroutines.launch
+
+class IconGalleryScreenModel : StateScreenModel<IconGalleryScreenModel.GalleryState>(GalleryState.Init) {
+
+    sealed class GalleryState {
+        data object Init : GalleryState()
+        data object Loading : GalleryState()
+        data object Empty : GalleryState()
+        data class Result(
+            val files: ArrayList<File>
+        ) : GalleryState()
+    }
+
+    private val context: Context = App.getContext()
+    val preferences: SharedPreferences = context.getSharedPreferences(App.PREFERENCES_NAME, MODE_PRIVATE)
+    private var files: ArrayList<File> = arrayListOf()
+
+    init {
+        mutableState.value = GalleryState.Loading
+        fetchAvatars()
+    }
+
+    private fun fetchAvatars() {
+        screenModelScope.launch {
+            files = api.files.fetchFilesByTag("icon")
+
+            if (files.isEmpty())
+                mutableState.value = GalleryState.Empty
+            else
+                mutableState.value = GalleryState.Result(files)
+        }
+    }
+}
