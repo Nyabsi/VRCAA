8d85962db186fe96e0bd13e09b8578e13a2ada46
Nyabsi <nyabsi@sovellus.cc>
2025-07-27 17:34:21 +0300

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cc.sovellus.vrcaa"
         minSdk = 27
         targetSdk = 35
-        versionCode = 200602
-        versionName = "2.6.2"
+        versionCode = 200603
+        versionName = "2.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
