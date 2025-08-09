8be5b0269be7f5e4b15e710a13d80b76341fba36
Nyabsi <nyabsi@sovellus.cc>
2025-08-09 21:33:14 +0300

    perf: implement R8 optimization to reduce memory + size usage
    (cherry picked from commit 4e7c67ca59a70ce581fa1ea7c048e587b1b0b5a4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
