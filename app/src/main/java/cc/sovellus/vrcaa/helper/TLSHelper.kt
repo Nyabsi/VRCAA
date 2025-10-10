d5923cc434e031bd51bb105edbc21f17ced73dd4
Nyabsi <nyabsi@sovellus.cc>
2025-10-10 15:25:46 +0300

    fix: fallback to tls 1.2 if 1.3 is not available (Android >=10)
    (cherry picked from commit bca9673a8ab99643a3482b121fa32a1486207c1a)

--- app/src/main/java/cc/sovellus/vrcaa/helper/TLSHelper.kt
+++ app/src/main/java/cc/sovellus/vrcaa/helper/TLSHelper.kt
@@ -1,19 +1,24 @@
 package cc.sovellus.vrcaa.helper;
 
 import java.security.KeyStore
+import java.security.NoSuchAlgorithmException
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
 class TLSHelper {
-    private val sslContext: SSLContext? = SSLContext.getInstance("TLSv1.3")
+    val sslContext: SSLContext = try {
+        SSLContext.getInstance("TLSv1.3")
+    } catch (_: NoSuchAlgorithmException) {
+        SSLContext.getInstance("TLSv1.2")
+    }
 
     init {
-        sslContext?.init(null, null, null)
+        sslContext.init(null, null, null)
     }
 
     fun getSSLContext(): SSLContext {
-        return sslContext!!
+        return sslContext
     }
 
     fun systemDefaultTrustManager(): X509TrustManager {
