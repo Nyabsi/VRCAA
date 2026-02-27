eb783c53c67419faa1292f3ec28e86e7ebbe57c6
Nyabsi <nyabsi@sovellus.cc>
2026-02-27 19:20:39 +0200

    chore: add ".dev" suffix to development builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ android {
             )
             signingConfig = signingConfigs.getByName("release")
         }
+        debug {
+            applicationIdSuffix = ".dev"
+        }
     }
 
     compileOptions {
