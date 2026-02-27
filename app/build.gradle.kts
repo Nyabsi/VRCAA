227a20bccd7f327c0217e063ac973e9744726edf
Nyabsi <nyabsi@sovellus.cc>
2026-02-27 19:11:33 +0200

    chore: add suffixes to packages to avoid Google Play statistics reports from other sources
    (cherry picked from commit f884a0b7bd4d028883b3d285856746f189f8f827)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,9 +94,15 @@ android {
         }
         create("quest") {
             dimension = "type"
+            applicationIdSuffix = ".quest"
         }
         create("pico") {
             dimension = "type"
+            applicationIdSuffix = ".pico"
+        }
+        create("nightly") {
+            dimension = "type"
+            applicationIdSuffix = ".nightly"
         }
     }
 }
